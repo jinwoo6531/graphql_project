a0a659e098fd4cb550c6802206cc5d97ef0f1c82
jinwoo <jintakk6531@gmail.com>
2020-12-18 20:29:45 +0900

    12/18

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+const { SECRET_KEY } = require('../../config');
+const { UserInputError } = require('apollo-server');
+
+module.exports = {
+  Mutation: {
+    register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      // TODO: Validate user Data
+      // TODO: Make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if(user) {
+          throw new UserInputError('Username is taken', {
+            error: {
+              username: 'This username is taken'
+            }
+          })
+      }
+      // TODO: hash password and create an auth token
+      password = await bcrypt.hash(password,12);
+
+      const newUser = new User({
+          email,
+          username,
+          password,
+          createdAt: new Date().toISOString()
+      });
+
+      const res = await newUser.save();
+      const token = jwt.sign({
+          id: res.id,
+          email: res.email,
+          username: res.username
+      }, SECRET_KEY, { expiresIn: '1h'});
+      return {
+          ...res.doc,
+            id:res._id,
+            token
+     }
+    },
+  },
+};
